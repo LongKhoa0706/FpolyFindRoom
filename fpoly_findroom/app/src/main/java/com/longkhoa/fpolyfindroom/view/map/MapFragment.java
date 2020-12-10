e7b488b4f3e14ae78cff98166be56c1fc792a1c5
Ngochan22 <nguyenthingochan504165@gmail.com>
2020-12-10 20:55:20 +0700

    add map

--- fpoly_findroom/app/src/main/java/com/longkhoa/fpolyfindroom/view/map/MapFragment.java
+++ fpoly_findroom/app/src/main/java/com/longkhoa/fpolyfindroom/view/map/MapFragment.java
@@ -9,12 +9,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.longkhoa.fpolyfindroom.R;
 
 public class MapFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.map_fragment, container, false);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
+        //async map
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                //when map is loaded
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        //when clicked on map
+                        //Initialize marker options
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        //set position of marker
+                        markerOptions.position(latLng);
+                        //set title of marker
+                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                        //remove all marker
+                        googleMap.clear();
+                        //manimating to zoom the marfer
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                latLng,10
+                        ));
+                        //add marker on map
+                        googleMap.addMarker(markerOptions);
+
+
+                    }
+                });
+
+            }
+        });
         return view;
     }
 }
